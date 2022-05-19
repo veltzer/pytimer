e84ebc725fbd8f3974f436b2b9d27684bc364a60
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 19:12:44 +0300

    new config

--- config/python.py
+++ config/python.py
@@ -1,7 +1,3 @@
-import config.project
-
-package_name = config.project.project_name
-
 install_requires = [
     "pytconf",
     "pylogconf",
@@ -19,8 +15,3 @@ dev_requires = [
     "pydmt",
     "Sphinx",
 ]
-
-python_requires = ">=3.10"
-
-test_os = ["ubuntu-22.04"]
-test_python = ["3.10"]
