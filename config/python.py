3ea1871669d0e554703a9cfda47d3df7b35ea3a6
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 02:53:54 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,15 +1,21 @@
-test_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = [
+    "pypitools",
+]
+install_requires: List[str] = []
+make_requires: List[str] = [
+    "pyclassifiers",
+    "pymakehelper",
+    "pydmt",
+]
+test_requires: List[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
     "flake8",
     "mypy",
 ]
-dev_requires = [
-    "pypitools",
-]
-make_requires = [
-    "pyclassifiers",
-    "pymakehelper",
-    "pydmt",
-]
+requires = config_requires + install_requires + make_requires + test_requires
